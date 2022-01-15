f3a39636c08491fbd8eeb3464236e4a309100b9a
Dukeboxz <u04sjj@gmail.com>
2022-01-15 17:46:21 +0000

    Added api project with api end point to get string stats

---
+++ TechTestApi/Controllers/StringProcessorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using TechTest;
+
+namespace TechTestApi.Controllers
+{
+  
+    [ApiController]
+    [Route("[controller]")]
+    public class StringProcessorController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetStringStats(string subject)
+        {
+            try
+            {
+                if(subject == null)
+                {
+                    return BadRequest(); 
+                }
+                StringStatsProcessor processor = new StringStatsProcessor();
+
+                var result = processor.Run(subject);
+
+                return new JsonResult(result);
+
+            }catch(Exception e)
+            {
+                return StatusCode(500);
+            }
+
+        }
+    }
+}
